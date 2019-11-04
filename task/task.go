7a427b6be1dea2569ee2e3dbb3ce80ebd0d66408
Tudyx <tudy.gourmelen@laposte.net>
2019-11-04 15:40:29 +0100

    Ajout majuscule au a NewTache car export

--- task/task.go
+++ task/task.go
@@ -2,13 +2,13 @@ package task
 
 var counterTask int
 
-// Tache : une tache contient la description d'une tache et d'un id
+// Task : une tache contient la description d'une tache et d'un id
 type Task struct {
 	description string
 	id          int
 }
 
-func newTask(description string) Task {
+func NewTask(description string) Task {
 	counterTask++
 	return Task{description, counterTask}
 }
