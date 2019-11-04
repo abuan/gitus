b9323e755e7b273eb5db4bed6df840e3d4d030af
Tudyx <tudy.gourmelen@laposte.net>
2019-11-04 15:28:47 +0100

    modification du nom du package task

---
+++ task/task.go
@@ -0,0 +1,14 @@
+package main
+
+var counterTask int
+
+// Tache : une tache contient la description d'une tache et d'un id
+type Task struct {
+	description string
+	id          int
+}
+
+func newTask(description string) Task {
+	counterTask++
+	return Task{description, counterTask}
+}
