84058a80bbb648e1050c6eba6e9fea968a435690
Tudyx <tudy.gourmelen@laposte.net>
2019-11-25 15:24:24 +0100

    ajout du champ auteur dans la structure userstory et mofication des methodes

--- userstory/userStory.go
+++ userstory/userStory.go
@@ -1,20 +1,22 @@
 package userstory
 
 import (
-	"github.com/abuan/gitus/task"
-	"time"
 	"fmt"
 	"strconv"
+	"time"
+
+	"github.com/abuan/gitus/task"
 )
 
 var counterUserStory int
 
 // UserStory : le type de base de notre projet
 type UserStory struct {
-	Name		  string
+	Name          string
 	Description   string
+	Author        string
 	ID            int
-	Effort 		  int
+	Effort        int
 	CreationDate  time.Time
 	ListTask      []task.Task
 	ListUserStory []UserStory
@@ -27,44 +29,45 @@ func (u *UserStory) SetDescription(s string) {
 func (u *UserStory) addTask(s string) {
 	u.ListTask = append(u.ListTask, task.NewTask(s))
 }
-func (u *UserStory) addUserStory(name , description string, effort int) {
-	u.ListUserStory = append(u.ListUserStory, NewUserStory(name,description,effort))
+func (u *UserStory) addUserStory(name, description, author string, effort int) {
+	u.ListUserStory = append(u.ListUserStory, NewUserStory(name, description, author, effort))
 }
 func (u *UserStory) getID() int {
 	return u.ID
 }
 
 // SetEffort : Affecte une valeur d'effort à une UserStroy
-func(u *UserStory) SetEffort(e int){
-	u.Effort = e;
+func (u *UserStory) SetEffort(e int) {
+	u.Effort = e
 }
 
 // SetCreationDate : Affecte une valeur date de création à une USerStory
-func(u *UserStory) SetCreationDate(t time.Time){
+func (u *UserStory) SetCreationDate(t time.Time) {
 	t.Format("2006-01-02 15:04:05")
 	u.CreationDate = t
 }
 
 // SetCreationDateNow : Affecte la date actuelle à une USerStory
-func(u *UserStory) SetCreationDateNow(){
+func (u *UserStory) SetCreationDateNow() {
 	t := time.Now()
 	t.Format("2006-01-02 15:04:05")
 	u.CreationDate = t
 }
 
 //NewUserStory : constructeur structure UserStory, incrémente l'id automatiquement
-func NewUserStory(name , description string, effort int) UserStory {
+func NewUserStory(name, description, author string, effort int) UserStory {
 	t := time.Now()
 	t.Format("2006-01-02 15:04:05")
-	return UserStory{name, description, 0, effort, t, nil, nil}
+	return UserStory{name, description, author, 0, effort, t, nil, nil}
 }
 
 // Display : Affiche le contenu de la US sur le terminal
-func(u *UserStory)Display(){
+func (u *UserStory) Display() {
 	fmt.Println("\n**************************************** User Story ****************************************")
-	fmt.Println("\tId:\t\t"+strconv.Itoa(u.ID))
-	fmt.Println("\tName :\t\t"+u.Name)
-	fmt.Println("\tEffort:\t\t"+strconv.Itoa(u.Effort))
-	fmt.Println("\tCreation Date :\t"+u.CreationDate.Format("2006-01-02 15:04:05"))
-	fmt.Println("\tDescription :\t"+u.Description)
+	fmt.Println("\tId:\t\t" + strconv.Itoa(u.ID))
+	fmt.Println("\tName :\t\t" + u.Name)
+	fmt.Println("\tEffort:\t\t" + strconv.Itoa(u.Effort))
+	fmt.Println("\tAuthor:\t\t" + u.Author)
+	fmt.Println("\tCreation Date :\t" + u.CreationDate.Format("2006-01-02 15:04:05"))
+	fmt.Println("\tDescription :\t" + u.Description)
 }
