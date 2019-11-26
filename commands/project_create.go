c6a2ae7e6edeb3e879ecf80ab25090fa1fbfd3c0
Alexis <juvin.alexis@gmail.com>
2019-11-26 18:48:56 +0100

    Modification des commandes "project"

--- commands/project_create.go
+++ commands/project_create.go
@@ -1,57 +1,51 @@
 package commands
 
-import(
-	"github.com/abuan/gitus/project"
-	"github.com/spf13/cobra"
-	"github.com/abuan/gitus/db"
-	"github.com/abuan/gitus/utils"
-	"fmt"
+import (
 	"errors"
+	"fmt"
+	"github.com/abuan/gitus/db"
+	"github.com/abuan/gitus/project"
+	"github.com/abuan/gitus/utils"
+	"github.com/spf13/cobra"
 	"strconv"
 )
 
-// Variable passée au flag 
+// Variable passée au flag
 var (
 	// COntient la liste des US à lier avec le projet
-	usList			[]int
+	usList []int
 )
 
 //Fonction créant une userstory à partir des arguments de la CLI
 func runCreateProject(cmd *cobra.Command, args []string) error {
 	//Création du projet
-	p := project.NewProject(args[0],"")
+	p := project.NewProject(args[0], "")
 
 	// Affectation description
-	if len(args)>1{
+	if len(args) > 1 {
 		var s string
 		for i := 1; i < len(args); i++ {
 			s += args[i] + " "
 		}
 		p.Description = s
 	}
-	//Connection avec la DB
-	err := db.InitDB()
-	defer db.CloseDB()
-	if err != nil{
-		return err
-	}
 
 	//Vérification des US passées en argument
 	usToLink := len(usList) > 0
-	if usToLink{
+	if usToLink {
 		//Suppression des doublons
 		usList = utils.RemoveDuplicates(usList)
 
 		//Sélection de tous les IDS des US
-		ids,err := db.TaskGetAllUserStoryID()
-		if err != nil{
+		ids, err := db.TaskGetAllUserStoryID()
+		if err != nil {
 			return err
 		}
 		// Vérifie si les ids passés en argument éxistent en BDD
-		exists,unknownValues :=utils.Contains(ids,usList)
+		exists, unknownValues := utils.Contains(ids, usList)
 		//Si des valeurs d'IDs n'éxistent pas alors on informe l'utilisateur
-		if !exists{
-			for _,val := range unknownValues{
+		if !exists {
+			for _, val := range unknownValues {
 				fmt.Print(strconv.Itoa(val) + " / ")
 			}
 			return errors.New("Les User Story liées aux IDs précédents n'existent pas")
@@ -60,14 +54,14 @@ func runCreateProject(cmd *cobra.Command, args []string) error {
 
 	//Sauvegarde en BDD du projet
 	projectID, err := db.TaskAddProject(&p)
-	if err != nil{
+	if err != nil {
 		return err
 	}
-	if usToLink{
+	if usToLink {
 		//Ajoute le liens entre les US et le projet
 		// Ce lien est fait via une table de jointure. Voir la méthodologie "Many to Many" liée à la gestion de BDD
-		err = db.TaskLinkUsToProject(usList,projectID)
-		if err != nil{
+		err = db.TaskLinkUsToProject(usList, projectID)
+		if err != nil {
 			return err
 		}
 	}
@@ -77,10 +71,12 @@ func runCreateProject(cmd *cobra.Command, args []string) error {
 
 // Var Cobra décrivant une commande CLI créant une UserStory
 var projectCreateCmd = &cobra.Command{
-	Use:     "create [<name>] <description>[...]",
-	Short:   "Create a new project.",
-	Args:	 cobra.MinimumNArgs(1),
-	RunE:    runCreateProject,
+	Use:      "create [<name>] <description>[...]",
+	Short:    "Create a new project.",
+	Args:     cobra.MinimumNArgs(1),
+	PreRunE:  connexionForData,
+	RunE:     runCreateProject,
+	PostRunE: deconnexionForData,
 }
 
 func init() {
