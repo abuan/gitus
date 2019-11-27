23187c230816a32a64da1057236bc33746ece02f
ANTOINE BUAN <antoine.buan6@gmail.com>
2019-11-27 19:11:08 +0100

    Modifie les fichiers d'instalation de la BDD

--- db/install/uninstall_BDD.bat
+++ db/install/uninstall_BDD.bat
@@ -1,7 +1,6 @@
 @ECHO OFF
-ECHO Suppression de la base de donnee "gitus" et de ses tables 
-ECHO Veuillez rentrer le mdp MySQL de l'utilisateur "root"
-mysql --user=root --password -s < uninstall_database.sql
+ECHO Suppression de la base de donnee "gitus" SQLite3 et de ses tables 
+del C:\sqlite\gitus.db
 ECHO La base de donnee a ete correctement desininstalee
 @ECHO ON
 pause
