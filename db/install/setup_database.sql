b450d12c9f7b5870b6523fd88701345a9abc774e
Tudyx <tudy.gourmelen@laposte.net>
2019-12-01 19:44:30 +0100

    ajout des champ auteur pour Userstory et Projet dans la table

--- db/install/setup_database.sql
+++ db/install/setup_database.sql
@@ -3,7 +3,8 @@ CREATE TABLE IF NOT EXISTS `Project` (
 	id INTEGER PRIMARY KEY AUTOINCREMENT,
 	name TEXT NOT NULL,
 	creation_date DATETIME,
-	descript TEXT
+	descript TEXT,
+	author TEXT
 );
 
 -- Creation de la table user_story
@@ -12,6 +13,7 @@ CREATE TABLE IF NOT EXISTS `UserStory`(
 	title text NOT NULL,
 	creation_date DATETIME,
 	descript TEXT,
+	author TEXT,
 	effort INTEGER
 );
 
@@ -33,9 +35,9 @@ CREATE TABLE IF NOT EXISTS `Project_structure` (
 );
 
 -- Ajoute des Items dans les tables
-INSERT INTO `Project` VALUES (1,"Default_Project1",datetime('now', 'localtime'),"Projet par defaut utilise pour les tests");
-INSERT INTO `Project` VALUES (2,"Default_Project2",datetime('now', 'localtime'),"Projet par defaut utilise pour les tests");
-INSERT INTO `UserStory` VALUES (1,"Default_US1",datetime('now', 'localtime'),"User Story par defaut utilise pour les tests",5);
-INSERT INTO `UserStory` VALUES (2,"Default_US2",datetime('now', 'localtime'),"User Story par defaut utilise pour les tests",8);
-INSERT INTO `UserStory` VALUES (3,"Default_US3",datetime('now', 'localtime'),"User Story par defaut utilise pour les tests",13);
+INSERT INTO `Project` VALUES (1,"Default_Project1",datetime('now', 'localtime'),"Projet par defaut utilise pour les tests","auteur 1");
+INSERT INTO `Project` VALUES (2,"Default_Project2",datetime('now', 'localtime'),"Projet par defaut utilise pour les tests","auteur 2");
+INSERT INTO `UserStory` VALUES (1,"Default_US1",datetime('now', 'localtime'),"User Story par defaut utilise pour les tests","auteur 1",5);
+INSERT INTO `UserStory` VALUES (2,"Default_US2",datetime('now', 'localtime'),"User Story par defaut utilise pour les tests","auteur 2",8);
+INSERT INTO `UserStory` VALUES (3,"Default_US3",datetime('now', 'localtime'),"User Story par defaut utilise pour les tests","auteur 3",13);
 INSERT INTO `Project_structure` VALUES(1,1),(1,2),(1,3),(2,1),(2,3);
